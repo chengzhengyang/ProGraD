2e32756e089f011b54687ca29d938aa171cc5cf6
remember9069 <79257059+remember9069@users.noreply.github.com>
2025-08-27 20:57:36 +0200

    Add files via upload

---
+++ util/utils.py
@@ -0,0 +1,16 @@
+# --------------------------------------------------------------------------------------------
+# Modified from CAFE codebase (https://github.com/dk-kim/CAFE_codebase)
+# Copyright (c) Kim, Dongkeun; Song, Youngkil; Cho, Minsu; Kwak, Suha . All Rights Reserved
+# --------------------------------------------------------------------------------------------
+
+import os
+
+
+def print_log(result_path, *args):
+    os.makedirs(result_path, exist_ok=True)
+
+    print(*args)
+    file_path = result_path + '/log.txt'
+    if file_path is not None:
+        with open(file_path, 'a') as f:
+            print(*args, file=f)
